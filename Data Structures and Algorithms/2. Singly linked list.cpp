251e359159bff143453673fa15a0550ac4dc98ab
ALIEX7664 <814574525@qq.com>
2020-07-24 13:03:05 +0800

    learn

--- Data Structures and Algorithms/2. Singly linked list.cpp
+++ Data Structures and Algorithms/2. Singly linked list.cpp
@@ -1,132 +1,189 @@
+/*
+*********************************************************************************************************
+*											单链表
+*********************************************************************************************************
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
-
-typedef struct Link {
-
-	char elem;
-	struct Link *next; //指向下一节点
-
-}link;
-
-link* initLink()
+typedef struct LNode
 {
-	link *p = NULL; //头指针
-	link *temp = (link*)malloc(sizeof(link)); //首元节点
-	temp->elem = 1;		//若有头节点无需此
-	temp->next = NULL;	//若有头节点无需此
-	p = temp; //指向头节点
-	for (int i = 2 ; i < SIZE ; i++)
-	{
-		link* a = (link*)malloc(sizeof(link));
-		a->elem = i;
-		a->next = NULL;
-		temp->next = a;
-		temp = temp->next;//使下次循环temp为a
-	}
-	
-	return p;
+	int data;
+	struct LNode *next;
+} LinkNode;
 
+//初始化单链表
+void InitList(LinkNode *&L)
+{
+	//头节点创建
+	L = (LinkNode *)malloc(sizeof(LinkNode));
+	L->next = NULL;
 }
 
-/* 带有头节点情况 */
-//void display(link* p) {
-//	link* temp = p;//将temp指针重新指向头结点
-//	//只要temp指针指向的结点的next不是Null，就执行输出语句。
-//	while (temp->next) {
-//		temp = temp->next;
-//		printf("%d", temp->elem);
-//	}
-//	printf("\n");
-//}
-
-
-void display(link* p)
+//销毁单链表
+void DestroyList(LinkNode *&L)
 {
-	link* temp = p; //定义链表存储p，防止p中途发生变化，导致数值输出错误
-	while (temp)
+	//获取头节点和首节点
+	LinkNode *pre = L, *p = L->next;
+
+	while (p != NULL)
 	{
-	
-			printf("%d", temp->elem);
-			temp = temp->next;
-			
+		free(pre);	   //释放前节点
+		pre = p;	   //前节点指针指向下个节点
+		p = pre->next; //下节点指针指向下下个节点
+	}
+}
+
+//判断单链表是否为空
+bool ListEmpty(LinkNode *L)
+{
+	//查看是否有首节点
+	return (L->next == NULL);
+}
+
+//查看单链表长度
+int ListLength(LinkNode *L)
+{
+	int n = 0;
+	LinkNode *p = L;
+
+	while (p->next != NULL)
+	{
+		n++;
+		p = p->next;
+	}
+	return n;
+}
+
+//显示链表中所有数据元素
+void DispList(LinkNode *L)
+{
+	LinkNode *p = L->next; //指向首节点
+
+	while (p != NULL)
+	{
+		printf("%d", p->data);
+		p = p->next;
 	}
 	printf("\n");
 }
 
-link* inserElem(link* p, int elem, int add) //p是原链表
+//根据序号获取元素值
+bool GetElem(LinkNode *L, int i, int &e)
 {
-	link *temp = p;
-	for (int i = 1;i < add; i++)
+	//由malloc创建的头节点不为空
+	LinkNode *p = L;
+	int j = 0;
+	while (j < i && p != NULL)
 	{
-		temp = temp->next;  //通过循环使之找到插入的上个结点
-		if (temp == NULL) {
-			printf("Insert invalid\n");
-			return p;
-		}
+		j++;
+		p = p->next;
 	}
-	
-	link* c = (link*)malloc(sizeof(link));
-	c  -> elem = elem;
-	c->next = temp->next; //使C的后节点指向前节点的后节点，实现插入
-	temp->next = c;	//使前节点指向C，以上两句顺序不能颠倒
 
-	return p;
-
+	if (p == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		e = p->data;
+		return true;
+	}
 }
 
-//p为原链表，elem表示被查找元素、
-int selectElem(link* p, int elem) { 
-	//新建一个指针t，初始化为头指针 p
-	link* t = p;
-	int i = 1;
-	//由于头节点的存在，因此while中的判断为t->next
-
-	while (t) {		//若有头节点改为t->next
-		if (t->elem == elem) {
-			return i;	//返回节点位置
-		}
-		t = t->next;
-		i++;
-	}
-	//程序执行至此处，表示查找失败
-	return -1;
-}
-
-
-//更新函数，其中，add 表示更改结点在链表中的位置，newElem 为新的数据域的值
-link* amendElem(link* p, int add, int newElem) {			//只需修改值，无需修改next指向
-	link* temp = p;	//指向首元节点
-
-	//temp = temp->next; //若有头节点要加上此句
-	//遍历到待更新结点
-
-	for (int i = 1; i < add; i++) {
-		temp = temp->next;
-	}
-	temp->elem = newElem;
-	return p;
-}
-
-int main()
+//根据元素值查找结点
+int LocateElem(LinkNode *L, int &e)
 {
-	link* p = initLink();
-	display(p);
+	int i = 1;
+	LinkNode *p = L->next;
 
+	//根据当前节点是否为空以及结果，判断是否继续遍历
+	while (p != NULL && p->data != e)
+	{
+		i++;
+		p = p->next;
+	}
 
-	p = inserElem(p, 9, 3);
-	display(p);
+	//根据是否存在结点，返回序号
+	if (p == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return i;
+	}
+}
 
-	p = inserElem(p, 3, 9);
-	display(p);
+//插入节点
+bool ListInsert(LinkNode *&L, int i, int e)
+{
+	int j = 0;
+	LinkNode *p = L, *s;
+	if (i <= 0)
+	{
+		return false;
+	}
 
-	int i = selectElem(p, 9);
-	printf("%d \n", i);
+	//根据序号和条件到达i-1位置
+	while (j < i - 1 && p != NULL)
+	{
+		j++;
+		p = p->next;
+	}
 
-	p = amendElem(p, 3, 7);
-	display(p);
+	//在查找到i-1过程中出现空结点，即i太大
+	if (p == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		//考虑中间插入与尾部插入
+		s = (LinkNode *)malloc(sizeof(LinkNode));
+		s->data = e;
+		p->next = s->next; //将p的后继指针域赋给s
+		p->next = s;	   //将s插入在链表中间
+		return true;
+	}
+}
+
+//删除结点
+bool ListDelete(LinkNode *&L, int i, int &e)
+{
+	int j = 0;
+	LinkNode *p = L, *q;
+
+	if (i <= 0)
+	{
+		return false;
+	}
+
+	while (j < i - 1 && p != NULL)
+	{
+		j++;
+		p = p->next;
+	}
+
+	if (p == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		q = p->next;
+		if (q == NULL)
+		{
+			return false;
+		}
+		e = q->data;
+		p->next = q->next;
+		free(q);
+		return true;
+	}
+}
+
+int main(void)
+{
 	return 0;
 }
-
-
